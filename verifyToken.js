b86a79f4524f300e720e50ba94ba2fd3ecd173e9
chau <chau1282001@gmail.com>
2022-12-26 21:41:24 +0700

    newCommit

--- verifyToken.js
+++ verifyToken.js
@@ -1,28 +1,29 @@
 
 
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(createError(401,"Token is not valid!" ));
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "You are not authenticated!"));
+    req.user = user;
+    next()
+  });
+};
+
+
 // import jwt from "jsonwebtoken";
 // import { createError } from "./error.js";
 
 // export const verifyToken = (req, res, next) => {
 //   const token = req.cookies.access_token;
-//   if (!token) return next(createError(401, "You are not authenticated!"));
-
+//   console.log(token)
 //   jwt.verify(token, process.env.JWT, (err, user) => {
 //     if (err) return next(createError(403, "Token is not valid!"));
 //     req.user = user;
 //     next()
 //   });
 // };
-
-
-import jwt from "jsonwebtoken";
-import { createError } from "./error.js";
-
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
-    req.user = user;
-    next()
-  });
-};
